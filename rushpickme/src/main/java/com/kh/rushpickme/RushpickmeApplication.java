55690364b14d37889045e5be14824a49b8470636
shw2336 <shw2336@naver.com>
2024-03-06 18:10:53 +0900

    kh-13 semi-pickmerush project 생성

---
+++ rushpickme/src/main/java/com/kh/rushpickme/RushpickmeApplication.java
@@ -0,0 +1,13 @@
+package com.kh.rushpickme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RushpickmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RushpickmeApplication.class, args);
+	}
+
+}
---
+++ rushpickme/src/test/java/com/kh/rushpickme/RushpickmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.rushpickme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RushpickmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
