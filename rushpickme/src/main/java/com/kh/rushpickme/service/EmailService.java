ef3dba5f8bca6872d9c9197197d3554dab614061
YJ2074 <yoojun2074@naver.com>
2024-03-12 15:57:56 +0900

    수정 업로드

---
+++ rushpickme/src/main/java/com/kh/rushpickme/service/EmailService.java
@@ -0,0 +1,12 @@
+package com.kh.rushpickme.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+		@Autowired
+		private ja
+	
+}
